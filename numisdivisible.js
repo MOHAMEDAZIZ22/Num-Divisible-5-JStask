a7abe10e71158e5f8a6f6bad4d947d6a8308f766
Mohamed Aziz <103256304+MOHAMEDAZIZ22@users.noreply.github.com>
2022-11-02 20:14:16 +0530

    Add files via upload

---
+++ numisdivisible.js
@@ -0,0 +1,12 @@
+let a = 10;
+
+if (a == 0) {
+    console.log("input is 0")
+}
+else if (a % 5 == 0) {
+    console.log("is divisible by 5");
+}
+else {
+    console.log("is not divisible by 5");
+}
+
